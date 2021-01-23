d90a6def317ac813f4017ca08ae0153199743736
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-01-22 21:56:45 -0300

    fix: work together component spacement

--- components/WorkTogether.js
+++ components/WorkTogether.js
@@ -9,14 +9,17 @@ const WorkTogether = () => {
         Que tal construir algo juntos?
       </h1>
 
-      <p className="text-lg md:text-xl leading-relaxed font-medium text-justify">
-        Adoro trabalhar com pessoas que compartilham a mesma paixão por tecnologia.
-        <br/>
-        Você é uma dessas pessoas?
-        <Link href="https://wa.link/tgjlij">
-          <a className="text-primary font-bold hover:opacity-70 transition">{' '}Vamos trabalhar juntos!</a>
-        </Link>
-      </p>
+      <div className="space-y-4 md:space-y-2">
+        <p className="text-lg md:text-xl leading-relaxed font-medium text-justify">
+          Adoro trabalhar com pessoas que compartilham a mesma paixão por tecnologia.
+        </p>
+        <p className="text-lg md:text-xl leading-relaxed font-medium text-justify">
+          Você é uma dessas pessoas?
+          <Link href="https://wa.link/tgjlij">
+            <a className="text-primary font-bold hover:opacity-70 transition">{' '}Vamos trabalhar juntos!</a>
+          </Link>
+        </p>
+      </div>
     </motion.section>
   )
 }
