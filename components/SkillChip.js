25dc028dd9c3ce1a7e7f676de06b41a6b4e13b58
Ruan Scherer <ruan.scherer@outlook.com>
2022-04-04 13:47:08 -0300

    feat: add skill chip animation

--- components/SkillChip.js
+++ components/SkillChip.js
@@ -1,7 +1,7 @@
 export const SkillChip = ({ skillName, backgroundColor }) => {
   return (
     <span
-      className="text-sm xl:text-base font-semibold rounded-full shadow px-4 py-2"
+      className="text-sm xl:text-base font-semibold rounded-full shadow px-4 py-2 transition transform hover:scale-105 hover:shadow-lg transition-transform duration-300 ease-in-out"
       style={{ backgroundColor: backgroundColor }}
     >
       {skillName}
