8aed86fcd3b7f839b498900415391f30a0af818c
Ruan Scherer <ruan.scherer@outlook.com>
2022-04-04 13:48:42 -0300

    feat: create medium card component

--- components/ExtrasSection.js
+++ components/ExtrasSection.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion"
 import { CustomContainer } from "../components/CustomContainer"
 import { opacityAnimation } from '../constants'
+import { MediumCard } from "./MediumCard"
 
 export const ExtrasSection = () => {
   return (
@@ -17,29 +18,7 @@ export const ExtrasSection = () => {
           </p>
         </div>
 
-        <div className="flex flex-col items-center justify-center col-span-3 xl:col-span-2 rounded-2xl p-5 m-auto border shadow-xl text-center transform hover:shadow-lg hover:translate-y-1 transition transition-250">
-          <div
-            className="w-20 md:w-24 xl:w-28 h-20 md:h-24 xl:h-28 mb-6 bg-cover bg-center rounded-full"
-            style={{ backgroundImage: "url(/images/medium-photo.jpeg)" }}
-          />
-          
-          <h3 className="mb-2 text-lg md:text-xl xl:text-2xl font-bold">
-            Ruan Scherer
-          </h3>
-          
-          <p className="mb-4 text-base md:text-lg xl:text-xl font-medium">
-            Full Stack Developer. Passionate about technology. Let's talk?
-          </p>
-          
-          <a
-            href="https://medium.com/@ruan.vscherer"
-            target="_blank"
-            className="text-gray-200 text-base md:text-lg font-medium font-medium shadow-lg rounded-full px-4 py-1 duration-200 hover:opacity-90"
-            style={{ backgroundColor: "var(--dark-blue-900)" }}
-          >
-            Go to Ruan's Medium
-          </a>
-        </div>
+        <MediumCard />
       </CustomContainer>
     </motion.section>
   )
